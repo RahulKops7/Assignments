993f30ecda50e085beaf7674eda0dd58d899869d
rockz <rockz@WIN-HTC4NMRTB5P>
2025-09-26 12:26:28 -0400

    Add project files.

---
+++ Assignment/Class3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Assignment
+{
+    class Class3
+    {
+        static void Main()
+        {
+            Console.Write("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is Even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is Odd.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Assignment/Week2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Week2
+    {
+        static void Main(string[] args)
+        {
+            int i = 2599;
+            char c = 'R';
+            string s = "RAHUL";
+            float f = 85.8f;
+            double d = 19.94;
+            long l = 9876543210;
+            bool b = true;
+
+            Console.WriteLine(i);
+            Console.WriteLine(c);
+            Console.WriteLine(s);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(l);
+            Console.WriteLine(b);
+
+            Console.Read();
+            
+
+
+        }
+    }
+}
